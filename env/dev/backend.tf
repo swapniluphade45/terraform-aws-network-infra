ed7b004a2d9476803f00f13084787d8a95d9d110
Swapnil Uphade <swapnil@example.com>
2026-01-22 01:51:37 +0530

    fmt: change the workflow file

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
 
     bucket = "terraform-statefile-swapnil45"
-    key = "prod/terraform.tfstate"
+    key = "dev/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
     use_lockfile = true
